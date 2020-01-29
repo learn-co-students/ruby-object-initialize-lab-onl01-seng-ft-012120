70f9ea3fb25e23ae1643e4f59cf307530341870c
Ian Marrs <ian.t.marrs@gmail.com>
2020-01-29 00:24:14 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,18 @@
 class Person
   
+  def initialize(name)
+    @name = name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
 end
 
 
-beyonce = Person.new 
+beyonce = Person.new("Beyonce") 
